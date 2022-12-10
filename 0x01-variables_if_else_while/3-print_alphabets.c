c1c31133f293bed70e72c5ad64597ac203a725a8
Quadotenshi <quadawosanya@gmail.com>
2022-12-09 17:02:22 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - function to print alphabet
+ * Return: to print cap and small
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		putchar(x);
+	for (x = 'A'; x <= 'Z'; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
