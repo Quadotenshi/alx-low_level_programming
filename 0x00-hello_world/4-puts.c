18587efcc559fcc2c915ccf9d8114f2ba72ce4db
Quadotenshi <quadawosanya@gmail.com>
2022-12-11 14:08:49 -0800

    hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - using the puts function
+ * Return: value 0
+ */
+int main(void)
+{
+	char myline[] = "Programming is like building a multilingual puzzle";
+
+	puts(myline);
+	return (0);
+}
