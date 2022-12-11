4800d366ed41e15e774965533c9139f33e43430c
Quadotenshi <quadawosanya@gmail.com>
2022-12-11 14:17:06 -0800

    hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - using the printf function
+ * Return: prints to the stdo
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
