e428c2c29f5809647de857341204cccadb69c0d3
Quadotenshi <quadawosanya@gmail.com>
2022-12-11 14:11:36 -0800

    hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-	char myline[] = "Programming is like building a multilingual puzzle";
+	char myline[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(myline);
 	return (0);
