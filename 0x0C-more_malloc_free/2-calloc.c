2c8eedf01f499aa222378f5b0ae4f82d3394916d
Quadotenshi <quadawosanya@gmail.com>
2023-01-25 15:13:47 -0800

    more malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/*
+ * _calloc - allocate mem for n numb of element
+ * @nmemb: number of elements
+ * @size: size of position of array
+ *
+ * Return: point void
+ */
+
+void *_calloc(unsighed int nmeb, unsigned int size)
+{
+	char *p;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb * size; i++)
+		p[i] = 0;
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,3 @@
+This is a README for my More Malloc projects.
+Here you will find projects around malloc(), realloc() and free()
+I do not understand most of what is done here at this time but i just hope i will be able to catch up with the C programming course cos i am very lost a the moment.
